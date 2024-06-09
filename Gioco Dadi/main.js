292f87269b46b36969b189a6c598aed1359124bf
Arela-Cristina <instagrandine@gmail.com>
2024-06-09 23:52:33 +0200

    Call button-user to js file
    Add an EventListener 'click' for button User,  to created an event which permit the user  choise a random number.
    
    Created an array called userChoices to then created an indexArray, and add the Math Floor and MathRandom function for make possible to the user choise  a random number between 1 and 6.

--- Gioco Dadi/main.js
+++ Gioco Dadi/main.js
@@ -1,2 +1,17 @@
 const buttonUser = document.getElementById('button-user');
-console.log(push)
+console.log(buttonUser);
+
+buttonUser.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    let userChoices = [1, 2, 3, 4, 5, 6];
+
+    let indexArray = Math.floor((Math.random() * 6) + 1);
+
+    userChoices = indexArray;
+
+    bottonUser = indexArray;
+    console.log('Soy el numero Random', bottonUser)
+
+})
+
