6bf76e617d8a0c9d933774b3a26959f8999a4740
Arela-Cristina <instagrandine@gmail.com>
2024-06-09 21:57:53 +0200

    Add event for sendButton Fix variable for sendButton, changued for let intead of const, because we have to change the value for get the Guest input.

--- main.js
+++ main.js
@@ -3,8 +3,14 @@ const emailGuest = document.querySelector(".emailInput");
 console.log(emailGuest);
 
 //chiediamo la mail ai guests
-const sendButton = document.querySelector(".sendButton");
+let sendButton = document.querySelector(".sendButton");
 console.log(sendButton);
+
+sendButton.addEventListener('click', function (event) {
+    event.preventDefault()
+    sendButton = emailGuest.value;
+    console.log('Soy el valor', sendButton);
+});
 
 const arrayGuest = ['simo@gmail.com', 'cri@gmail.com', 'mono@gmail.com'];
 
