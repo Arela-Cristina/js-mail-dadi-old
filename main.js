821b880d4f33c51a44c85378e1db016e89409050
Arela-Cristina <instagrandine@gmail.com>
2024-06-09 23:10:50 +0200

    Fix variable for sendButton, changue for let instead of const, because we have to change the value for get the Guest input.
    Move arrayGuest and conditions inside of sendButton.addEventListener
    
    created a variable let outside the for loop, preventing it from repeating itself three times.
    
    Code work, the script is  capable of validating the input to compare it with the list and operate based on the conditions

--- main.js
+++ main.js
@@ -7,15 +7,21 @@ let sendButton = document.querySelector(".sendButton");
 console.log(sendButton);
 
 sendButton.addEventListener('click', function (event) {
-    event.preventDefault()
-    sendButton = emailGuest.value;
-    console.log('Soy el valor', sendButton);
+    event.preventDefault();
+    console.log('Soy el valor', emailGuest.value);
+
+    const arrayGuest = ['simo@gmail.com', 'cri@gmail.com', 'mono@gmail.com'];
+
+    let messaggio = 'No estas invitado';
+
+    for (let i = 0; i < arrayGuest.length; i++) {
+        let guest = arrayGuest[i];
+        // console.log('Siamo gli invitati', guest);
+
+        if (guest === emailGuest.value) {
+            messaggio = 'Estas invitado♥';
+        }
+    }
+    console.log(messaggio);
 });
 
-const arrayGuest = ['simo@gmail.com', 'cri@gmail.com', 'mono@gmail.com'];
-
-for (let i = 0; i < arrayGuest.length; i++) {
-    const guest = arrayGuest[i];
-    console.log('Somos los invitados', guest);
-
-}
